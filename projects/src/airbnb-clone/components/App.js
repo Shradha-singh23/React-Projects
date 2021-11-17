8ad8209d20e03c44939546499a6b31e30f6b53eb
Shradha-singh23 <shradha23singh@gmail.com>
2021-11-17 13:46:48 +0530

    Passing props in components

--- projects/src/airbnb-clone/components/App.js
+++ projects/src/airbnb-clone/components/App.js
@@ -3,13 +3,26 @@ import Navbar from "./Navbar";
 import "./App.css";
 import Hero from "./Hero";
 import Card from "./Card";
+import experience from "../data";
 
 export default function App(){
+    const cards = experience.map(item => {
+        return(
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                review={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    });
     return(
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            {cards}
         </div>
     )
 }
--- projects/src/airbnb-clone/components/Card.js
+++ projects/src/airbnb-clone/components/Card.js
@@ -1,19 +1,19 @@
 import React from "react";
-import katie from "../images/katie.png";
 import star from "../images/Star.png";
 
-export default function Card(){
+export default function Card(props){
+    console.log(props)
     return(
         <div className="card">
-            <img src={katie} alt="katie" className="card--image"/>
+            <img src={require(`../images/${props.img}.png`).default} alt="katie" className="card--image"/>
             <div className="card--stats">
                 <img src={star} alt="star" className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
