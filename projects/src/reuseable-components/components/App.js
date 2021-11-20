528f687a7a1454293d17cd68a92173625da60536
Shradha-singh23 <shradha23singh@gmail.com>
2021-11-20 17:56:31 +0530

    worked with props as: Rounded corners, size and bgcolor

--- projects/src/reuseable-components/components/App.js
+++ projects/src/reuseable-components/components/App.js
@@ -4,7 +4,32 @@ import Button from "./Button"
 export default function App(){
     return(
         <div>
-            <Button bgcolor="secondary" isRoundedCorners={false}> Click Me! </Button>
+            <Button 
+                bgcolor="primary" 
+                isRoundedCorners={true} 
+                size="xs"
+            > 
+                Click Me! 
+            </Button>
+            <Button 
+                bgcolor="secondary" 
+                isRoundedCorners={false} 
+                size="sm"
+            > 
+                Click Me! 
+            </Button>
+            <Button 
+                bgcolor="tertiary" 
+                isRoundedCorners={true} 
+                size="md"
+            > 
+                Click Me! 
+            </Button>
+            <Button 
+                size="lg"
+            > 
+                Click Me! 
+            </Button>
         </div>
     )
 }
--- projects/src/reuseable-components/components/Button.js
+++ projects/src/reuseable-components/components/Button.js
@@ -6,27 +6,46 @@ const backgroundColors = {
     secondary: "#CBAACB",
     tertiary: "#744C24"
 }
+const variants = {
+    xs: {
+        padding: "5px",
+        width: "5%"
+    },
+    sm: {
+        padding: "10px",
+        width: "8%"
+    },
+    md: {
+        padding: "10px",
+        width: "12%"
+    },
+    lg: {
+        padding: "12px",
+        width: "15%"
+    }
+}
 
 export default function Button({
     children,
     bgcolor,
     buttonSize,
-    isRoundedCorners
+    isRoundedCorners,
+    size
 })
 {
     return(
         <button
             style={{
                 fontWeight: 400,
+                padding: variants[size].padding,
                 cursor: "pointer",
                 transition: "transform 0.3s ease",
                 backgroundColor: backgroundColors[bgcolor] || backgroundColors.default,
                 color:"white",
                 marginLeft: "20px",
                 marginTop:"50px",
-                fontSize: "30px",
-                width: "20%",
-                height: "60px",
+                fontSize: "20px",
+                width: variants[size].width,
                 borderRadius: isRoundedCorners ? "10px" : "0px"
             }}
         >
